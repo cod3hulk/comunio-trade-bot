2a4551f273e509a802a3773b9956f899b20998eb
Tomas Ave <tomave82@gmail.com>
2016-02-09 09:03:42 +0100

    Refactored script to encapsulate request logic

--- main.py
+++ main.py
@@ -3,45 +3,56 @@ import requests
 import json
 import argparse
 
-def computeOffer(player):
-    quotedPrice = player['quotedPrice']
-    recommendedPrice = player['recommendedPrice']
-    return int(recommendedPrice + ((quotedPrice - recommendedPrice) * 0.224))
+class Bot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-def playerId(player):
-    return "price[%s]" % player['playerId']
+    def login(self):
+        self.session = requests.Session()
+        data = { 'login': self.username, 'pass': self.password }
+        r = self.session.post("http://www.comunio.de/login.phtml", data)
 
-def placeOffer(players, session, communityId):
-    for player in players:
-        offerId = playerId(player)
-        offer = computeOffer(player)
-        data = {offerId:offer, 'action':'addOffer', 'makeoffer_x':'33', 'placedInCommunity':communityId}
-        session.post("http://www.comunio.de/exchangemarket.phtml", data)
-        print 'Placed offer [player:%s] [offer:%s]' % (player['playerName'], offer)
+    def players(self):
+        r = self.session.get('http://www.comunio.de/exchangemarket.phtml')
+        self.communityId = BeautifulSoup(r.text).find("input", {"name":"placedInCommunity"})["value"]
+
+        r = self.session.get('http://www.comunio.de/rest/exchangeMarketService.php')
+        return json.loads(r.text)
+
+    def placeOffer(self, players):
+        for player in players:
+            offerId = self.playerId(player)
+            offer = self.computeOffer(player)
+            data = {offerId:offer, 'action':'addOffer', 'makeoffer_x':'33', 'placedInCommunity':self.communityId}
+            self.session.post("http://www.comunio.de/exchangemarket.phtml", data)
+            print 'Placed offer [player:%s] [offer:%s]' % (player['playerName'], offer)
+
+    def computeOffer(self, player):
+        quotedPrice = player['quotedPrice']
+        recommendedPrice = player['recommendedPrice']
+        return int(recommendedPrice + ((quotedPrice - recommendedPrice) * 0.224))
+
+    def playerId(self, player):
+        return "price[%s]" % player['playerId']
+
 
 def shouldBuy(player):
     return ((player['quotedPrice'] - player['recommendedPrice']) > 20000) and not player['bid']
 
 def main():
-    #parse commandline arguments
+    # parse commandline arguments
     parser = argparse.ArgumentParser(description='Place comunio exchange offers automatically')
     parser.add_argument("username", help='username used for login')
     parser.add_argument("password", help='password used for login')
     args = parser.parse_args()
 
-    # login
-    s = requests.Session()
-    data = { 'login': args.username, 'pass': args.password }
-    r = s.post("http://www.comunio.de/login.phtml", data)
+    bot = Bot(args.username, args.password)
+    bot.login()
+    players = bot.players()
 
-    # open exchange market
-    r = s.get('http://www.comunio.de/exchangemarket.phtml')
-    communityId = BeautifulSoup(r.text).find("input", {"name":"placedInCommunity"})["value"]
-
-    ## get current players on transfer market
-    r = s.get('http://www.comunio.de/rest/exchangeMarketService.php')
-    players = json.loads(r.text)
     playersToBuy = [player for player in players if shouldBuy(player)]
-    placeOffer(playersToBuy, s, communityId)
+
+    bot.placeOffer(playersToBuy)
 
 main()
